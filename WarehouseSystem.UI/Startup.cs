c5d1de6736fe1db2f294ec4d51dd92df6556393a
Šarūnas Valiukas <sarunas.valiukas@gmail.com>
2017-10-14 19:24:48 +0300

    Init commit.

---
+++ WarehouseSystem.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WarehouseSystem.UI.Startup))]
+namespace WarehouseSystem.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
